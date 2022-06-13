1bfdc5329ff94c25040ec729c94b2bedddd7833f
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-06-13 07:58:02 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+// middleware imports
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("e-commerce-api");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+// connect to database
+const connectDB = require("./db/connect");
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URL);
+		app.listen(port, () => {
+			console.log(`server running on port ${port}`);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
