171a23a98327e1e6043db4b58c1461da3568acd8
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-06-13 15:33:08 +0000

    logout

--- controller/authController.js
+++ controller/authController.js
@@ -17,11 +17,32 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-	res.send("login");
+	const { email, password } = req.body;
+	if (!email || !password) {
+		throw new BadRequestError("please provide all details");
+	}
+
+	const user = await User.findOne({ email });
+	if (!user) {
+		throw new NotFoundError("no user with such credentials");
+	}
+
+	const isPasswordMatched = user.comparePassword(user.password);
+	if (!isPasswordMatched) {
+		throw new NotFoundError("no user with such credentials");
+	}
+
+	const tokenPayload = { name: user.name, userId: user._id, role: user.role };
+	attacheCookieResponse({ res, tokenPayload });
+	res.status(StatusCodes.OK).json({ user });
 };
 
 const logout = async (req, res) => {
-	res.send("logout");
+	res.cookie("token", "", {
+		httpOnly: true,
+		expires: new Date(Date.now()),
+	});
+	res.status(StatusCodes.OK).json({ msg: "user logged out!" });
 };
 
 module.exports = { register, login, logout };
